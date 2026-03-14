fcb5e34f0d6f0053572cd4e76a09d12a30e01adb
Ganavi sk <ganavisrinivask16@gmail.com>
2026-03-14 22:31:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+body{
+font-family:Arial;
+background:#f4f6f8;
+text-align:center;
+}
+
+button{
+padding:8px 15px;
+margin:10px;
+border:none;
+background:#007bff;
+color:white;
+border-radius:5px;
+cursor:pointer;
+}
